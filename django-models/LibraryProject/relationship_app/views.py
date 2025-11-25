287f612f379dbe6519f706c7f3776634117338c1
Alex <akinjisealex@gmail.com>
2025-11-25 15:58:00 -0800

    Add all

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Function-based view: list all books
 def list_books(request):
