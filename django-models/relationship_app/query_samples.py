dc98939a0d75342c85edb7c00050caf4d9f3a996
Alex <akinjisealex@gmail.com>
2025-11-25 14:17:00 -0800

    Add all

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,37 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Query all books by a specific author
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        return books
+    except Author.DoesNotExist:
+        return []
+
+
+# 2. List all books in a library
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
+
+
+# 3. Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except Library.DoesNotExist:
+        return None
+    except Librarian.DoesNotExist:
+        return None
+
+
+# SAMPLE INTERACTIVE TEST (optional)
+if __name__ == "__main__":
+    print(get_books_by_author("J.K. Rowling"))
+    print(get_books_in_library("Central Library"))
+    print(get_librarian_for_library("Central Library"))
