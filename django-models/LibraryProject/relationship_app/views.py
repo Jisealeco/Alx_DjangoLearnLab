ec7d33b995ccaa1cdc161bc19ee7197b1aeeb121
Alex <akinjisealex@gmail.com>
2025-11-25 15:55:26 -0800

    Add all

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from .models import Library
 from django.views.generic import DetailView
 
 # Function-based view: list all books
